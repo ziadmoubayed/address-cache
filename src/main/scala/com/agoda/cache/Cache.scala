d950fa7eed6397c71f913b92bff95b1fd2b123e4
Ziad <ziadmoubayed@gmail.com>
2018-11-28 11:14:12 -0500

    second commit

---
+++ src/main/scala/com/agoda/cache/Cache.scala
@@ -0,0 +1,11 @@
+package com.agoda.cache
+
+/*
+ * The AddressCache has a max age for the elements it's storing, an add method
+ * for adding elements, a remove method for removing, a peek method which
+ * returns the most recently added element, and a take method which removes
+ * and returns the most recently added element.
+ */
+trait Cache[T] {
+
+}
